ad9feeb93ecc9f1328492b53c26b600769d3f3d6
Bekelly13 <bekell5@ilstu.edu>
2025-03-07 19:58:05 -0600

    Update Project1AID.ino

--- Project1AID.ino
+++ Project1AID.ino
@@ -1,7 +1,9 @@
-int buzzerPin = 9;  // Set your buzzer pin here
+int buzzerPin = 9;  // Buzzer connected to pin 9
+int ledPin = 8;     // LED connected to pin 8
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);  // Start serial communication at 9600 baud rate
 }
 
@@ -10,9 +12,13 @@ void loop() {
     char received = Serial.read();  // Read the serial input
     
     if (received == '1') {
-      tone(buzzerPin, 1000);  // Emit a 1000 Hz tone on the buzzer
-    } else if (received == '0') {
-      noTone(buzzerPin);  // Stop the tone
+      tone(buzzerPin, 1000);   // Turn on buzzer at 1000 Hz
+      digitalWrite(ledPin, LOW); // Turn off LED
+    } 
+    else if (received == '0') {
+      noTone(buzzerPin);        // Turn off buzzer
+      digitalWrite(ledPin, HIGH); // Turn on LED
     }
   }
 }
+
